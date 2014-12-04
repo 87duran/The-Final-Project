de2e56f823350d5c796d02bb5d4c34d069823472
87duran <87duran@gmail.com>
2014-12-04 12:28:28 -0700

    getting localhost to be 5000

--- public/app/mainService.js
+++ public/app/mainService.js
@@ -4,13 +4,13 @@ app.service('mainService', function($http) {
     this.getListofUsers = function() {
         return $http({
             method: 'GET',
-            url: 'http://localhost:3000/users'
+            url: 'http://localhost:5000/users'
         })
     }
 
     this.addNewUser = function(gameMobtest_user, callback) {
         console.log('new user function')
-        return $http.post('http://localhost:3000/users', {gameMobtest_user: gameMobtest_user})
+        return $http.post('http://localhost:5000/users', {gameMobtest_user: gameMobtest_user})
         .success(function(data, status, headers, config){
             console.log(data);
             callback(data);
@@ -23,13 +23,13 @@ app.service('mainService', function($http) {
     this.getListofsessions = function() {
         return $http({
             method: 'GET',
-            url: 'http://localhost:3000/sessions'
+            url: 'http://localhost:5000/sessions'
         })
     }
 
     this.addNewSession = function(test_session, callback) {
         console.log('new session function');
-        return $http.post('http://localhost:3000/sessions', {test_session: test_session})
+        return $http.post('http://localhost:5000/sessions', {test_session: test_session})
             .success(function(data, status, headers, config){
                 console.log(data);
                 callback(data);
