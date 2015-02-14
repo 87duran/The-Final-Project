5f2acae352768bcb5bccbd1c594c2b3ba0ab34b9
87duran <87duran@gmail.com>
2015-02-14 13:20:39 -0700

    CSS
    changed background image and some coloring

--- server.js
+++ server.js
@@ -14,25 +14,25 @@ app.set('port', (process.env.PORT || 5000));
 
 var pg = require('pg');
 
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-    var query = client.query('SELECT * FROM gamemobtest_users');
-
-    query.on('row', function(row) {
-        console.log(JSON.stringify(row));
-    });
-});
-
-//app.get('/db', function (request, response) {
-//    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-//        client.query('SELECT * FROM test_sessions', function(err, result) {
-//            done();
-//            if (err)
-//            { console.error(err); response.send("Error " + err); }
-//            else
-//            { response.send(result.rows); }
-//        });
+//pg.connect(process.env.DATABASE_URL, function(err, client) {
+//    var query = client.query('SELECT * FROM gamemobtest_users');
+//
+//    query.on('row', function(row) {
+//        console.log(JSON.stringify(row));
 //    });
-//})
+//});
+
+app.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM test_sessions', function(err, result) {
+            done();
+            if (err)
+            { console.error(err); response.send("Error " + err); }
+            else
+            { response.send(result.rows); }
+        });
+    });
+})
 
 var Sequelize = require('sequelize')
     , sequelize = new Sequelize('test', 'cduran87', 'postgres', {
